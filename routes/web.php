297de4d6a1a25393c220ddb0cbad5ef44b6fe130
zelphyx <rizqy.astiko19@gmail.com>
2023-11-21 08:25:38 +0700

    PAS 21-11-2023

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('home',["title" => "home"]);
+});
 Route::get('/home', function () {
     return view('home',["title" => "home"]);
 });
 Route::get('/keyboard', [\App\Http\Controllers\KeyboardController::class,'index']);
 Route::get('/mouse', [\App\Http\Controllers\MouseController::class,'index']);
+Route::get('/monitor', [\App\Http\Controllers\MonitorController::class,'index']);
+
+
 Route::get('/mouse/detail/{mouse}',[\App\Http\Controllers\MouseController::class,'show']);
 Route::get('/keyboard/detail/{keyboard}',[\App\Http\Controllers\KeyboardController::class,'show']);
+Route::get('/monitor/detail/{monitor}',[\App\Http\Controllers\MonitorController::class,'show']);
 
